556dad931bd976f813184daf0c926d9d6f4d3fdc
NivedhReddy <nivedhreddypingili@gmai.com>
2026-05-14 00:25:13 +0530

    Added JWT token routes

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from apps.analyzer.views import ResumeAnalyzeView, JobMatchAnalyzeView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -19,4 +23,6 @@ urlpatterns = [
     path('api/mock/', include('apps.mock_interview.urls')),
     path('api/mentor/', include('apps.mentor.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
